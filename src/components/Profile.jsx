2dc571cd4c04aa4e6db3f34e79ed5c29b2857b6d
Shohruh-tuit <51286131+Shohruh-tuit@users.noreply.github.com>
2019-10-03 10:19:54 +0500

    My first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQfWDuN0WJnyE65UxVVU1HffCt_l_Bp__4IJulfKmOd0cChyByZ" />
+      </div>
+      <div>ava + description</div>
+      <div>
+        My posts
+        <div>New posts</div>
+      </div>
+      <div>
+        <div>post 1</div>
+        <div>post 2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
