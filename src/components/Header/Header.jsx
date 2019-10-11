79de8670c5f36a6005b43e6eb6323bdcfe25d3a0
Shohruh-tuit <51286131+Shohruh-tuit@users.noreply.github.com>
2019-10-11 10:58:12 +0500

    First_Project

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import s from './Header.module.css';
+
+const Header = () => {
+    return(
+        <header className={s.header}>
+        <img src='https://s1.logaster.com/static/v3/img/products/logo.png'/>
+        <b>Start_Coding.Uz</b>
+      </header>
+        );
+}
+
+export default Header;
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from './styles/Profile.module.css';
 
 const Profile = () => {
   return (
-    <div className="content">
+    <div className={s.content}>
       <div>
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQfWDuN0WJnyE65UxVVU1HffCt_l_Bp__4IJulfKmOd0cChyByZ" />
       </div>
@@ -11,9 +12,9 @@ const Profile = () => {
         My posts
         <div>New posts</div>
       </div>
-      <div>
-        <div>post 1</div>
-        <div>post 2</div>
+      <div className={s.posts}>
+        <div className={s.item}>post 1</div>
+        <div className={s.item}>post 2</div>
       </div>
     </div>
   );
