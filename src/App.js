d57d20a0f9a70f5e36bca03c7b066ecf4ee9a0ef
Shohruh-tuit <51286131+Shohruh-tuit@users.noreply.github.com>
2019-10-15 04:15:57 +0500

    prosti

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from "react";
 import "./App.css";
+import s from "../src/components/Dialogs/Dialogs.module.css";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Dialogs from "./components/Dialogs/Dialogs";
 import Profile from "./components/Profile/Profile";
+import Settings from "./components/Settings/Settings";
+import {BrowserRouter, Route} from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <Profile />
-    </div>
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        <div className="app-wrapper-content">
+          <Route path='/dialogs' component={Dialogs} />
+          <Route path='/profile' component={Profile} />
+          <Route path='/news' component={News} />
+          <Route path='/music' component={Music} />
+          <Route path='/settings' component={Settings} />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 };
 
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
   return (
-    <div className={s.content}>
+    <div>
       <div>
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQfWDuN0WJnyE65UxVVU1HffCt_l_Bp__4IJulfKmOd0cChyByZ" />
       </div>
