71fb3f9bf117542622aabcf271acacb3786580ab
Shohruh-tuit <51286131+Shohruh-tuit@users.noreply.github.com>
2019-11-15 01:47:04 +0500

    prosti

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,39 +1,47 @@
-import React from 'react';
-import s from './Header.module.css';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import React from "react";
+import s from "./Header.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    faFacebook,
-    faInstagram,
-    faTelegramPlane,
-    faStackOverflow,
+  faFacebook,
+  faInstagram,
+  faTelegramPlane,
+  faStackOverflow
 } from "@fortawesome/free-brands-svg-icons";
 
 const Header = () => {
-    return(
-        <header className={s.header}>
-        <div className={s.danniy}>
+  return (
+    <header className={s.header}>
+      <div className={s.danniy}>
         {/* <div className={s.moon}><br/><br/><b>STATIC_X</b></div> */}
-       <a href='http://localhost:3000/profile'> <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRxdSfYf9DW9koH2nyLuNve99m5uWpAXye4jWTv4vbUYwaY464J" alt=""/>
-       </a>
-          <div className={s.item}>
+        <a href="http://localhost:3000/profile">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRxdSfYf9DW9koH2nyLuNve99m5uWpAXye4jWTv4vbUYwaY464J"
+          />
+        </a>
+        <div className={s.item}>
           <FontAwesomeIcon icon={faStackOverflow} size="2px" />
-           <b>Tel:</b> (90) 975-76-51
-          </div>
-          <div className={s.item}>
-          <FontAwesomeIcon icon={faTelegramPlane} size="2px" />
-           <b>Email:</b> Shohruhmasharipov49@gmail.com
-          </div>
-          <div className={s.item}>
-          <FontAwesomeIcon icon={faFacebook} size="2px" />
-          <b>Facebook:</b> <a href="https://www.facebook.com/shohruh.okayer?ref=bookmarks">  Shohruh.com</a> 
-          </div>
-          <div className={s.item}>
-          <FontAwesomeIcon icon={faInstagram} size="2px" />
-          <b>Instagram:</b> <a href="https://instagram.com/i_shoxruh"> i_shoxruh</a> 
-          </div> 
+          <b>Tel:</b> (90) 975-76-51
         </div>
-      </header>
-        );
-}
+        <div className={s.item}>
+          <FontAwesomeIcon icon={faTelegramPlane} size="2px" />
+          <b>Email:</b> Shohruhmasharipov49@gmail.com
+        </div>
+        <div className={s.item}>
+          <FontAwesomeIcon icon={faFacebook} size="2px" />
+          <b>Facebook:</b>{" "}
+          <a href="https://www.facebook.com/shohruh.okayer?ref=bookmarks">
+            {" "}
+            Shohruh.com
+          </a>
+        </div>
+        <div className={s.item}>
+          <FontAwesomeIcon icon={faInstagram} size="2px" />
+          <b>Instagram:</b>{" "}
+          <a href="https://instagram.com/i_shoxruh"> i_shoxruh</a>
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
