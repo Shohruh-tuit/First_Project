a494168bf5f65b4ae88539184c1ff13c1be70b8c
Shohruh-tuit <51286131+Shohruh-tuit@users.noreply.github.com>
2020-03-17 02:10:39 +0500

    Karantin

--- src/App.js
+++ src/App.js
@@ -18,13 +18,7 @@ const App = (props) => {
         <Header />
         <Navbar />
         <div className="app-wrapper-content">
-          {/* <Route path="/dialogs" component={Dialogs} />
-          <Route path="/profile" component={Profile} />
-          <Route path="/news" component={News} />
-          <Route path="/music" component={Music} />
-          <Route path="/settings" component={Settings} />  */}
-
-          <Route path="/dialogs" render={() => <Dialogs />} />
+          <Route path="/dialogs" render={() => <Dialogs dialogs={props.dialogs} messages = {props.messages} />} />
           <Route path="/profile" render={() => <Profile posts={props.posts} />} />
           <Route path="/news" render={() => <News />} />
           <Route path="/music" render={() => <Music />} />
--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 let posts = [
-    {id: 1, messages: "Hi, How are you", countLike: 23},
-    {id: 2, messages: "It's my first post on React!", countLike: 48},
-    ]
-
-ReactDOM.render(<App post={posts}/>, document.getElementById('root'));
+  { id: 1, messages: "Hi, How are you", countLike: 23 },
+  { id: 2, messages: "It's my first post on React!", countLike: 48 }
+];
+let dialogs = [
+  { id: 1, name: "Shohruh" },
+  { id: 2, name: "Alisher" },
+  { id: 3, name: "Jovlon" },
+  { id: 4, name: "Humoyun" },
+  { id: 5, name: "Suxrob" }
+];
+let messages = [
+    { id: 1, message: "Salom" },
+    { id: 2, message: "Ishlar yaxwimi" },
+    { id: 3, message: "yaxwi raxmat!!!" }
+  ];
+ReactDOM.render(<App posts={posts} dialogs={dialogs} messages={messages}/>, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
