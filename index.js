23783c0ec42cc07d3cf3eb503840b76eacf70cbd
Shohruh-tuit <51286131+Shohruh-tuit@users.noreply.github.com>
2020-03-17 00:51:46 +0500

    Karantin

---
+++ index.js
@@ -0,0 +1,26 @@
+// let oldArray = ['Shohruh', 'Baxodir', 'Mansur', 'Malohat'];
+
+// let newArray = oldArray.map(name => {
+//     return `${name}`
+// })
+
+// console.log(newArray)
+
+
+
+
+// Umir aka
+
+// let Menu = {
+//     hotdog: 3000,
+//     hamburger: 5000,
+//     lavash: 12000
+// }
+
+// const Plus = (n) =>{
+//    soni = 4;
+//    console.log(soni*Object.values(Menu)[n]) 
+// }
+
+// Plus(0);
+
